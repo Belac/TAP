0192bd96f60acfdaa425d3dd70269967ffa39b4b
Belac <calebhoulding@gmail.com>
2018-05-01 11:29:09 +0100

    First Edition

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<header>
+		<title>The Tap Shop</title>
+		<link rel="stylesheet" type="text/css" href="storecss.css">
+	</header>
+	<body id="homepage">
+		<div>
+			<h1>The Tap Shop</h1>
+			<p id="motto">Let the spring water flow through your home, with style.</p>
+
+		</div>
+		<div id="tapFactsSection">
+			<p id="tapFacts">SIGN UP FOR TAP FACTS</p>
+			<form action="submit.php" method="post" accept-charset="utf-8">
+				<input type="name" name="firstname" placeholder="first name" class="formInput" style="width: 60%; height: 80px"><<br>
+				<input type="name" name="lastname" placeholder="last name" class="formInput" style="width: 60%; height: 80px"><<br>
+				<input type="text" name="email" placeholder="email" class="formInput" style="width: 60%; height: 80px"><<br>
+				<button type="submit" name="submitBut" class="button">subscribe</button><<br>
+			</form>
+			<p id="Emmett" style="color: #555; font-size: 15px; text-align: right; font-family: Verdana; vertical-align: bottom; text-align: left; margin-left: 20px;">Property of Emmett & Co.</p>
+		</div>
+	</body>
